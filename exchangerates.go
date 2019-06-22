7cbb9c981802a1108f3a7f3ec1fa4712bcec526d
George Moschovitis <gmosx@reizu.com>
2019-06-22 12:44:36 +0300

    apply optional parameters of GetExchangeRates

--- exchangerates.go
+++ exchangerates.go
@@ -4,13 +4,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
+	"time"
 )
 
 // GetExchangeRatesOptions contains optional parameters for the GetExchangeRates
 // method.GetExchangeRatesOptions
 type GetExchangeRatesOptions struct {
-	date   string
-	strict bool
+	Date   time.Time
+	Strict bool
 }
 
 type exchangeRatesResponse struct {
@@ -22,6 +24,21 @@ type exchangeRatesResponse struct {
 // https://developers.ripple.com/data-api.html#get-exchange-rates
 func (c *Client) GetExchangeRates(base, counter string, opts *GetExchangeRatesOptions) (string, *Response, error) {
 	path := fmt.Sprintf("/exchange_rates/%s/%s", base, counter)
+
+	if opts != nil {
+		v := url.Values{}
+
+		if !opts.Date.IsZero() {
+			v.Set("date", FormatTime(opts.Date))
+		}
+
+		if opts.Strict {
+			v.Set("strict", "true")
+		}
+
+		path += "?" + v.Encode()
+	}
+
 	res, err := c.Do(http.MethodGet, path)
 
 	data := &exchangeRatesResponse{}
---
+++ util.go
@@ -0,0 +1,9 @@
+package xrplda
+
+import "time"
+
+// FormatTime formats a time for passing to the XRPL Data API endpoints.
+// https://xrpl.org/data-api.html#timestamps
+func FormatTime(t time.Time) string {
+	return t.UTC().Format(time.RFC3339)
+}
